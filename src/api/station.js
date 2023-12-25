e960890cd3dc5d9f38c33551cb198e86d645e832
张柒 <12909657+love-open-source-white-whore@user.noreply.gitee.com>
2023-12-25 15:44:50 +0800

    update

---
+++ src/api/station.js
@@ -0,0 +1,24 @@
+import request from '@/utils/request'
+
+export function getStationName() {
+  return request({
+    url: '/station/stationName',
+    method: 'get'
+  })
+}
+
+export function getStationList(trainNo) {
+  return request({
+    url: '/station/list',
+    method: 'get',
+    params: { trainNo }
+  })
+}
+
+export function searchStation(start_station, end_station) {
+  return request({
+    url: '/station/search',
+    method: 'get',
+    params: { start_station, end_station }
+  })
+}
